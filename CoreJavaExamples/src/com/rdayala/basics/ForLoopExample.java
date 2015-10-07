1065b49e8ae7b7ca487eb1989ab8a7ed0270fdfa
Raghunath Dayala <rndayala@gmail.com>
2015-10-07 08:39:40 +0530

    Add ForLoopExample.java

---
+++ CoreJavaExamples/src/com/rdayala/basics/ForLoopExample.java
@@ -0,0 +1,12 @@
+package com.rdayala.basics;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("the value is : " + i);
+		}
+
+	}
+
+}
