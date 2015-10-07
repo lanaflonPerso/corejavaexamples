9ec88b248c49804fdeeb369685ddd832b146ee7b
Raghunath Dayala <rndayala@gmail.com>
2015-10-07 08:44:47 +0530

    Add DoWhileExample.java

---
+++ CoreJavaExamples/src/com/rdayala/basics/DoWhileExample.java
@@ -0,0 +1,14 @@
+package com.rdayala.basics;
+
+public class DoWhileExample {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			System.out.println("the value is : " + i);
+			i++;
+		}while(i <= 5);
+
+	}
+
+}
