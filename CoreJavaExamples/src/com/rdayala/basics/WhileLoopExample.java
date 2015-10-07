a611e7e182f2fb373ebb15b51f9957e7a44c49a5
Raghunath Dayala <rndayala@gmail.com>
2015-10-07 08:41:27 +0530

    Add WhileLoopExample.java

---
+++ CoreJavaExamples/src/com/rdayala/basics/WhileLoopExample.java
@@ -0,0 +1,14 @@
+package com.rdayala.basics;
+
+public class WhileLoopExample {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while(i <= 5) {
+			System.out.println("the value is : " + i);
+			i++;
+		}
+
+	}
+
+}
