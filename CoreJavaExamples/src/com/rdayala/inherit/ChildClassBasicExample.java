d05a415128a690bcbee88095d4419d68b7338090
Raghunath Dayala <rndayala@gmail.com>
2015-10-08 11:49:50 +0530

    Add initial inheritance examples

---
+++ CoreJavaExamples/src/com/rdayala/inherit/ChildClassBasicExample.java
@@ -0,0 +1,37 @@
+package com.rdayala.inherit;
+
+// Parent class with no constructors.
+// Compiler automatically creates a default constructor
+
+class B {
+	
+}
+
+// Child class with no constructors defined.
+// Compiler automatically creates a default constructor.
+// Inside the child class default constructor, which is created by compiler, 
+// it calls super class default constructor as the first statement.
+
+class C extends B {
+	
+	public void printSomething() {
+		System.out.println("C object is created");
+	}
+}
+
+public class ChildClassBasicExample {
+
+	public static void main(String[] args) {
+		
+		/**
+		 * If your child class doesn't contain any constructors,
+		 * the compiler automatically generates a default constructor. 
+		 * This child class default constructor internally
+		 * calls super class default constructor, super(), as the first statement.
+		 */
+		C cObj = new C();
+		cObj.printSomething();
+
+	}
+
+}
